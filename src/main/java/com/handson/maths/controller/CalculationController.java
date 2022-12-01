abf0d8246bbb95e7ba9f7a360bb8117e2a9a10e8
Rajkumar Katanor <katanor.raj@gmail.com>
2022-12-01 05:50:46 +0530

    changed mapping

--- src/main/java/com/handson/maths/controller/CalculationController.java
+++ src/main/java/com/handson/maths/controller/CalculationController.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.handson.maths.model.Request;
@@ -22,36 +22,36 @@ public class CalculationController {
 	
 	
 
-	@PostMapping("/add")
-	public ResponseEntity<Response> getAddition(@RequestBody Request req) {
-		int res = calculations.add(req.getA(), req.getB());
+	@GetMapping("/add")
+	public ResponseEntity<Response> getAddition(@RequestParam int a, @RequestParam int b) {
+		int res = calculations.add(a,b);
 		Response resp = new Response();
 		resp.setResult(res);
 		resp.setName("Add");
 		return new ResponseEntity<Response>(resp, HttpStatus.OK);
 	}
 
-	@PostMapping("/sub")
-	public ResponseEntity<Response> getSubsraction(@RequestBody Request req) {
-		int res = calculations.sub(req.getA(), req.getB());
+	@GetMapping("/sub")
+	public ResponseEntity<Response> getSubsraction(@RequestParam int a, @RequestParam int b) {
+		int res = calculations.sub(a,b);
 		Response resp = new Response();
 		resp.setResult(res);
 		resp.setName("Sub");
 		return new ResponseEntity<Response>(resp, HttpStatus.OK);
 	}
 
-	@PostMapping("/mul")
-	public ResponseEntity<Response> getMultiplication(@RequestBody Request req) {
-		int res = calculations.mul(req.getA(), req.getB());
+	@GetMapping("/mul")
+	public ResponseEntity<Response> getMultiplication(@RequestParam int a, @RequestParam int b) {
+		int res = calculations.mul(a,b);
 		Response resp = new Response();
 		resp.setResult(res);
 		resp.setName("Mul");
 		return new ResponseEntity<Response>(resp, HttpStatus.OK);
 	}
 
-	@PostMapping("/div")
-	public ResponseEntity<Response> getDivision(@RequestBody Request req) {
-		int res = calculations.div(req.getA(), req.getB());
+	@GetMapping("/div")
+	public ResponseEntity<Response> getDivision(@RequestParam int a, @RequestParam int b) {
+		int res = calculations.div(a,b);
 		Response resp = new Response();
 		resp.setResult(res);
 		resp.setName("Div");
